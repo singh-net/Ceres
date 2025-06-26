416e14b384ffcfc6f18daed7bf86b79a0edd81f2
singh-net <sam63156@hotmail.com>
2025-06-26 15:45:42 -0700

    modal

--- client/src/app/dashboard/dashboard.ts
+++ client/src/app/dashboard/dashboard.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  TemplateRef,
+  WritableSignal,
+} from '@angular/core';
+
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css'
+  styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  private modalService = inject(NgbModal);
+  closeResult: WritableSignal<string> = signal('');
 
+  open(content: TemplateRef<any>) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult.set(`Closed with: ${result}`);
+        },
+        (reason) => {
+          this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    switch (reason) {
+      case ModalDismissReasons.ESC:
+        return 'by pressing ESC';
+      case ModalDismissReasons.BACKDROP_CLICK:
+        return 'by clicking on a backdrop';
+      default:
+        return `with: ${reason}`;
+    }
+  }
 }
